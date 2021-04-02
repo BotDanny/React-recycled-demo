dea27411d149d47e61c00de494d9a87a9b5982c5
BotDanny <dy2yang@DESKTOP-KP6H4SP.localdomain>
2021-04-02 15:34:51 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import FixedList from './FixedList';
 
+function randInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRamdomRowHeightAndColumn(dataLength: number) {
+  const heights: number[] = [];
+  const columns: number[] = [];
+  let nextDataIndex = 0;
+  for (let i = 0; nextDataIndex < dataLength; i++) {
+    heights[i] = randInt(60, 140);
+    const column = randInt(1 ,4);
+    const tempNextDataIndex = nextDataIndex + column;
+    columns[i] = tempNextDataIndex > dataLength? dataLength - nextDataIndex : column;
+    nextDataIndex = tempNextDataIndex
+  }
+
+  return [heights, columns];
+}
+
 function App() {
-  const data = Array(50).fill(null).map((_, index) => index)
+  const data = Array(50).fill(null).map((_, index) => index);
+  const [heights, columns] = generateRamdomRowHeightAndColumn(data.length)
   return (
     <div className="App">
       <FixedList
         height={350}
         data={data}
         rowHeight={100}
+        rowHeights={heights}
+        rowColumns={columns}
       />
     </div>
   );
--- src/FixedList.tsx
+++ src/FixedList.tsx
@@ -1,36 +1,140 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 interface props {
-    height: number;
-    data: any[];
-    rowHeight: number;
-    rowHeights?: number[];
-    column?: number;
-    rowColumns?: number[];
+  height: number;
+  data: any[];
+  rowHeight: number;
+  rowHeights: number[];
+  column?: number;
+  rowColumns?: number[];
+  additionalRenderedRow?: number;
 }
 
 interface state {
-    renderedRowAbsoluteIndex: number[];
-    topRenderedRowRelativeIndex: number;
-    loadingState: boolean[];
+  renderedRowAbsoluteIndex: number[];
+  topRenderedRowRelativeIndex: number;
+  loadingState: boolean[];
+}
+
+function calculateRowPositions(rowHeights: number[]) {
+  let prev = 0;
+  const rowPositions: number[] = [];
+  const rowCount = rowHeights.length;
+  for (let i = 0; i < rowCount; i++) {
+    if (i === 0) rowPositions[i] = 0;
+    else rowPositions[i] = prev;
+    prev += rowHeights[i];
+  }
+  return rowPositions;
+}
+
+type RowToDataIndexMap = { [key: string]: [number, number] };
+function mapRowIndexToDataIndex(rowColumns: number[], totalNumOfData: number) {
+  const rowCount = rowColumns.length;
+  let prevEndDataIndex = 0;
+  let map: RowToDataIndexMap = {};
+  for (let i = 0; i < rowCount; i++) {
+    const newEndDataIndex = Math.min(
+      prevEndDataIndex + rowColumns[i],
+      totalNumOfData
+    );
+    map[i] = [prevEndDataIndex, newEndDataIndex];
+    prevEndDataIndex = newEndDataIndex;
+  }
+  return map;
 }
 
 export default class FixedList extends React.PureComponent<props, state> {
-    rowPositions: number[];
-    rowToDataIndexMap: {[key: string]: [number, number]};
+  rowPositions: number[];
+  rowToDataIndexMap: { [key: string]: [number, number] };
+  fullHeight: number;
+  initialArrayTemplate: null[];
+  totalNumOfRenderedRows: number;
 
+  validateProps = () => {
+    const { rowHeight, rowHeights, column, rowColumns, data } = this.props;
+    if (rowColumns) {
+      if (
+        rowColumns.reduce((acc, current) => acc + current, 0) !== data.length
+      ) {
+        throw Error(
+          "The total number of data item calculated from rowColumns does not match the length of your input data"
+        );
+      }
+      if (rowColumns.length !== rowHeights.length) {
+        throw Error(
+          "The number of rows provided from rowHeights does not match the number of rows provided from rowColumns"
+        );
+      }
+    } else if (column) {
+      const rows = Math.ceil(data.length / column);
+      if (rows !== rowHeights.length) {
+        throw Error(
+          "The number of rows provided from rowHeights does not match the number of rows calculated from column"
+        );
+      }
+    } else if (rowHeights.length !== data.length) {
+      throw Error(
+        "The number of rows provided from rowHeights does not match the number of rows calculated from your input data"
+      );
+    }
+  };
 
-    constructor(props: props) {
-        super(props);
+  constructor(props: props) {
+    super(props);
 
+    const {
+      rowHeight,
+      rowHeights,
+      column,
+      rowColumns,
+      data,
+      height,
+      additionalRenderedRow,
+    } = props;
+
+    this.validateProps()
+
+    const calculatedRowColumns: number[] = rowColumns
+      ? rowColumns
+      : column
+      ? Array(rowHeights.length).fill(column)
+      : Array(rowHeights.length).fill(1);
+
+    this.rowToDataIndexMap = mapRowIndexToDataIndex(
+      calculatedRowColumns,
+      data.length
+    );
+    this.rowPositions = calculateRowPositions(rowHeights);
+
+    const totalRows = rowHeights.length;
+    if (this.rowToDataIndexMap[totalRows - 1][1] !== data.length) {
+      throw Error(
+        "The total number of data item calculated from rowHeights does not match the length of your input data"
+      );
     }
 
+    const numOfVisibleRow = Math.ceil(height / rowHeight);
+    const numOfInvisibleRowOnEachDirection =
+      additionalRenderedRow || Math.ceil(numOfVisibleRow / 2);
+    this.totalNumOfRenderedRows =
+      numOfVisibleRow + numOfInvisibleRowOnEachDirection * 2;
+    if (this.totalNumOfRenderedRows < totalRows)
+      this.totalNumOfRenderedRows = totalRows;
+    this.initialArrayTemplate = Array(this.totalNumOfRenderedRows).fill(null);
 
-    render() {
-        return (
-            <div>
+    this.fullHeight = rowHeights.reduce((acc, current) => acc + current, 0);
 
-            </div>
-        )
-    }
+    this.state = {
+      renderedRowAbsoluteIndex: this.initialArrayTemplate.map(
+        (_, index) => index
+      ),
+      loadingState: this.initialArrayTemplate.map(() => false),
+      topRenderedRowRelativeIndex: 0,
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
 }
