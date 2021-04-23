02862a38d09c42ef04c5805f5dc27af115e02d4d
Daniel Yang <dy2yang@uwaterloo.ca>
2021-04-23 18:44:22 -0400

    added stats.js

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,27 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import Stats from "stats.js";
+var stats = new Stats();
+stats.dom.style.right = "0";
+stats.dom.style.left = "auto";
+stats.showPanel( 0 );
+document.body.appendChild( stats.dom );
+
+function animate() {
+
+	stats.begin();
+
+	// monitored code goes here
+
+	stats.end();
+
+	requestAnimationFrame( animate );
+
+}
+console.log(stats.dom)
+
+requestAnimationFrame( animate );
 ReactDOM.render(
   <React.StrictMode>
     <Router>
